6d4fc56bcdc5e81f9dd2e7897d1b7e31dafe57b1
Jake Gardner <gardner.761@gmail.com>
2025-11-21 20:36:07 -0600

    updated recipe data

--- src/custom-types/Recipe.ts
+++ src/custom-types/Recipe.ts
@@ -1,3 +1,5 @@
+import { GCs } from "./GlobalConstants";
+
 export interface JobData {
     ActiveRecipeIndex: number; // used with Machine.RecipeStore.Recipes array
     JobName: string; // this is the identifier for the specific job
@@ -12,20 +14,50 @@ export interface JobData {
     JobComplete: boolean;
 }
 
+export interface ApplicatorSetpoint {
+    speed: number; // speed robot uses while applying liner
+    squeegeeDiameter: number; // mm of Inner Diameter
+    zOffset: number; // mm of Distance relative to false bottom
+    ballValveOn: boolean; // whether ball valve is on or off at this setpoint
+}
+
+export const exampleApplicatorSetpoints: ApplicatorSetpoint[] = [
+    { speed: 100, squeegeeDiameter: 30.0, zOffset: -10.0, ballValveOn: true },
+    { speed: 100, squeegeeDiameter: 30.0, zOffset: 100.0, ballValveOn: true },
+    { speed: 150, squeegeeDiameter: 28.5, zOffset: 200.0, ballValveOn: true },
+    { speed: 200, squeegeeDiameter: 27.0, zOffset: 300.0, ballValveOn: false },
+];
+
+export const initialApplicatorSetpoints: ApplicatorSetpoint[] = Array(GCs.MAX_APPLICATOR_SETPOINTS).fill({
+    speed: 0,
+    squeegeeDiameter: 0.0,
+    zOffset: 0.0,
+    ballValveOn: false,
+});
+
 export interface RecipeData {
-    linerType: string; // display purposes only
+    index: number; // index in RecipeStore.Recipes array
+
+    tubeTypeId: number;
+    tubeTypeString: string; // display purposes only
     tubeOuterDiameterMax: number; // mm, used for robot offsets in fixture
-    tubePartNumber: string; // display purposes only
-    setpointSpeeds: number[]; // speed robot uses while applying liner
-    setpointDiameters: number[]; // mm of Inner Diameters
-    setpointDistances: number[]; // mm of Distances relative to false bottom
-    applicatorToolColorId: ApplicatorTools;
-    applicatorToolColorString: string;
+    falseBottomStaysOpen: boolean; // this also determines if only the tall fixture is used or not
+
+    linerPotPressure: number; // PSI
+    linerType: string; // display purposes only
+
+    applicatorSetpoints: ApplicatorSetpoint[]; // speed robot uses while applying liner
+    applicatorToolId: ApplicatorTools;
+    applicatorToolString: string;
+    cleanerId: number; // cleaner tool used before applying liner
+    cleanerString: string;
+
     weighingFingerId: WeighingFingers;
     weighingFingerString: string;
+
     numCameras: number; // number of cameras needed to take photos of a tube
     cameraIds: number[]; // used with NumCameras to determine how photos are taken
-    falseBottomStaysOpen: boolean; // this also determines if only the tall fixture is used or not
+    cameraSpeeds: number[]; // speed settings for robot for each camera 
 }
 
 export enum CameraIds {
@@ -35,18 +67,14 @@ export enum CameraIds {
     CAMERA_3 = 3,
 }
 
-export function getCameraIdString(cameraId: CameraIds): string {
-    switch (cameraId) {
-        case CameraIds.CAMERA_1:
-            return "Camera 1";
-        case CameraIds.CAMERA_2:
-            return "Camera 2";
-        case CameraIds.CAMERA_3:
-            return "Camera 3";
-        default:
-            return "None";
-    }
-}
+export const cameraIdToStringMap = new Map<CameraIds, string>([
+    [CameraIds.NONE, "None"],
+    [CameraIds.CAMERA_1, "Camera 1"],
+    [CameraIds.CAMERA_2, "Camera 2"],
+    [CameraIds.CAMERA_3, "Camera 3"],
+]);
+
+export const cameraStringToIdMap = new Map<string, CameraIds>(Array.from(cameraIdToStringMap.entries()).map(([id, str]) => [str, id]));
 
 export enum TubeTypes {
     NONE = 0,
@@ -58,6 +86,19 @@ export enum TubeTypes {
     TYPE_1755_037 = 6,
     TYPE_1517_038 = 7
 }
+
+export const tubeTypeIdToStringMap = new Map<number, string>([
+    [TubeTypes.NONE, "None"],
+    [TubeTypes.TYPE_11726650, "11726650"],
+    [TubeTypes.TYPE_1809_370, "1809-370"],
+    [TubeTypes.TYPE_1809_126, "1809-126"],
+    [TubeTypes.TYPE_1340_38, "1340-38"],
+    [TubeTypes.TYPE_3811302, "3811302"],
+    [TubeTypes.TYPE_1755_037, "1755-037"],
+    [TubeTypes.TYPE_1517_038, "1517-038"],
+]);
+
+export const tubeTypeStringToIdMap = new Map<string, number>(Array.from(tubeTypeIdToStringMap.entries()).map(([id, str]) => [str, id]));
 
 export function getTubeTypeString(tubeType: TubeTypes): string {
     switch (tubeType) {
@@ -96,51 +137,92 @@ export enum ApplicatorTools {
 }
 
 
+
+export const applicatorToolIdToStringMap = new Map<ApplicatorTools, string>([
+    [ApplicatorTools.NONE, "None"],
+    [ApplicatorTools.RED_37_INCH, "Red 3/7 Inch"],
+    [ApplicatorTools.BLUE_F16, "Blue F16"],
+    [ApplicatorTools.GREEN_SMALL, "Green Small"],
+    [ApplicatorTools.ORANGE_LARGE, "Orange Large"],
+    [ApplicatorTools.YELLOW_MEDIUM, "Yellow Medium"],
+]);
+
+export const applicatorToolStringToIdMap = new Map<string, ApplicatorTools>(Array.from(applicatorToolIdToStringMap.entries()).map(([id, str]) => [str, id]));
+
+export enum Cleaners {
+    NONE = 0,
+    SMALL = 1,
+    MEDIUM = 2,
+    LARGE = 3
+}
+
+export const cleanerIdToStringMap = new Map<Cleaners, string>([
+    [Cleaners.NONE, "None"],
+    [Cleaners.SMALL, "Small"],
+    [Cleaners.MEDIUM, "Medium"],
+    [Cleaners.LARGE, "Large"],
+]);
+
+export const cleanerStringToIdMap = new Map<string, Cleaners>(Array.from(cleanerIdToStringMap.entries()).map(([id, str]) => [str, id]));
+
 export enum WeighingFingers {
     NONE = 0,
-    RED_MEDIUM = 1,
-    ORANGE_LARGE = 2,
-    ORANGE_LARGE_2 = 3,
-    BLUE_SMALL = 4,
-    BLUE_SMALL_2 = 5,
-    BLUE_SMALL_3 = 6,
-    BLUE_SMALL_4 = 7
+    BLUE_SMALL = 1,
+    RED_MEDIUM = 2,
+    ORANGE_LARGE = 3,
 }
+
+export const weighingFingerIdToStringMap = new Map<WeighingFingers, string>([
+    [WeighingFingers.NONE, "None"],
+    [WeighingFingers.RED_MEDIUM, "Red Medium"],
+    [WeighingFingers.ORANGE_LARGE, "Orange Large"],
+    [WeighingFingers.BLUE_SMALL, "Blue Small"],
+]);
+
+export const weighingFingerStringToIdMap = new Map(
+    Array.from(weighingFingerIdToStringMap.entries()).map(([id, str]) => [str, id])
+);
 
 
 export const exampleRecipe: RecipeData = {
     linerType: "CKU",
+    linerPotPressure: 50,
+    index: 1,
+    tubeTypeId: TubeTypes.TYPE_1340_38,
+    tubeTypeString: "1340-38",
     tubeOuterDiameterMax: 38.1,
-    tubePartNumber: "11726650",
-    setpointSpeeds: [100, 150, 200],
-    setpointDiameters: [30.0, 28.5, 27.0],
-    setpointDistances: [100.0, 200.0, 300.0],
-    applicatorToolColorId: ApplicatorTools.RED_37_INCH,
-    applicatorToolColorString: "Red 3/7 Inch",
+    falseBottomStaysOpen: false,
+
+    applicatorSetpoints: exampleApplicatorSetpoints,
+    applicatorToolId: ApplicatorTools.RED_37_INCH,
+    applicatorToolString: "Red 3/7 Inch",
+    cleanerId: Cleaners.MEDIUM,
+    cleanerString: "Medium",
     weighingFingerId: WeighingFingers.RED_MEDIUM,
     weighingFingerString: "Red Medium",
     numCameras: 2,
     cameraIds: [CameraIds.CAMERA_1, CameraIds.CAMERA_2],
-    falseBottomStaysOpen: false,
+    cameraSpeeds: [50, 75],
+    
 };
 
-const MAX_APPLICATOR_SETPOINTS = 10;
-const MAX_CAMERAS = 3;
-const MAX_NUM_RECIPES = 20;
-
 export const initialRecipe: RecipeData = {
+    index: 0,
     linerType: "",
     tubeOuterDiameterMax: 0.0,
-    tubePartNumber: "",
-    setpointSpeeds: Array(MAX_APPLICATOR_SETPOINTS).fill(0),
-    setpointDiameters: Array(MAX_APPLICATOR_SETPOINTS).fill(0),
-    setpointDistances: Array(MAX_APPLICATOR_SETPOINTS).fill(0),
-    applicatorToolColorId: ApplicatorTools.NONE,
-    applicatorToolColorString: "",
+    tubeTypeId: TubeTypes.NONE,
+    tubeTypeString: "",
+    linerPotPressure: 0,
+    applicatorSetpoints: initialApplicatorSetpoints,
+    applicatorToolId: ApplicatorTools.NONE,
+    applicatorToolString: "",
+    cleanerId: Cleaners.NONE,
+    cleanerString: "",
     weighingFingerId: WeighingFingers.NONE,
     weighingFingerString: "",
     numCameras: 0,
-    cameraIds: Array(MAX_CAMERAS).fill(CameraIds.NONE),
+    cameraIds: Array(GCs.MAX_NUM_CAMERAS).fill(CameraIds.NONE),
+    cameraSpeeds: Array(GCs.MAX_NUM_CAMERAS).fill(0),
     falseBottomStaysOpen: false,
 };
 
@@ -151,5 +233,5 @@ export interface RecipeStore {
 
 export const initialRecipeStore: RecipeStore = {
     numRecipes: 2,
-    recipes: Array(MAX_NUM_RECIPES).fill(exampleRecipe),
+    recipes: Array(GCs.MAX_NUM_RECIPES).fill(exampleRecipe),
 };
