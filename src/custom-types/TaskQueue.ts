9b9334737b9c140be07c4d8c0a4d280f9c2adf4b
kuriousdesign <gardner.761@gmail.com>
2026-05-29 19:24:54 -0600

    task queue changes

--- src/custom-types/TaskQueue.ts
+++ src/custom-types/TaskQueue.ts
@@ -5,52 +5,96 @@ import { DeviceConstants } from "./BaseDevice";
 // (
 // 	NONE := 0, //do not remove or change this
 // 	RECOVER_ROBOT :=1,
-// 	LOAD_PARTS:=2,
-// 	START_POT_PRESSURIZATION:=10, //param0: explain what the parameters do here, param1: continue to explain each of them
-// 	PRE_WEIGH_TUBES:=20, //no params:
+// 	CALIBRATE_GRIPPER:=2,
+// 	ACTIVATE_RECIPE:=3,
+// 	PERFORM_CHANGEOVER:=4,
+// 	CALIBRATE_SQUEEGEE:=5,
+// 	ENTER_JOB_DATA:=6,
+// 	LOAD_RAW_TUBES:=10,
+// 	ENTER_BATCH_DATA:=11,
+// 	START_POT_PRESSURIZATION:=15,
+// 	PRIME_APPLICATOR:=17,
+// 	PRE_WEIGH_TUBES:=20,
 // 	APPLY_LINER :=30,
+// 	BRUSH_LINER := 35,
+// 	START_POT_DEPRESSURIZATION:=39,
 // 	PHOTOGRAPH_TUBES :=40,
 // 	POST_WEIGH_TUBES:=50,
-// 	START_POT_DEPRESSURIZATION:=60,
-// 	WAIT_IMAGE_INSPECTION:=70,
-// 	UNLOAD_PARTS:=80
+// 	PREP_FOR_UNLOAD:=60,
+// 	REVIEW_VIDEOS:=70,
+// 	SAVE_BATCH_DATA:=80,
+// 	UNLOAD_PARTS:=85,
+// 	DONE:=1000
 // );
 // END_TYPE
 export enum Priorities {
     NONE = 0, // do not remove or change this
-    LOAD_PARTS = 2,
-    START_POT_PRESSURIZATION = 10,
+    RECOVER_ROBOT = 1,
+    CALIBRATE_GRIPPER = 2,
+    ACTIVATE_RECIPE = 3,
+    PERFORM_CHANGEOVER = 4,
+    CALIBRATE_SQUEEGEE = 5,
+    ENTER_JOB_DATA = 6,
+    LOAD_RAW_TUBES = 10,
+    ENTER_BATCH_DATA = 11,
+    START_POT_PRESSURIZATION = 15,
+    PRIME_APPLICATOR = 17,
     PRE_WEIGH_TUBES = 20,
     APPLY_LINER = 30,
+    BRUSH_LINER = 35,
     PHOTOGRAPH_TUBES = 40,
     POST_WEIGH_TUBES = 50,
-    START_POT_DEPRESSURIZATION = 60,
-    WAIT_IMAGE_INSPECTION = 70,
-    UNLOAD_PARTS = 80
+    PREP_FOR_UNLOAD = 60,
+    REVIEW_VIDEOS = 70,
+    SAVE_BATCH_DATA = 80,
+    UNLOAD_PARTS = 85,
+    DONE = 1000
 }
 
 export function priorityToString(priority: Priorities): string {
     switch (priority) {
         case Priorities.NONE:
             return "None";
-        case Priorities.LOAD_PARTS:
-            return "Load Parts";
+        case Priorities.RECOVER_ROBOT:
+            return "Recover Robot";
+        case Priorities.CALIBRATE_GRIPPER:
+            return "Calibrate Gripper";
+        case Priorities.ACTIVATE_RECIPE:
+            return "Activate Recipe";
+        case Priorities.PERFORM_CHANGEOVER:
+            return "Perform Changeover";
+        case Priorities.CALIBRATE_SQUEEGEE:
+            return "Calibrate Squeegee";
+        case Priorities.ENTER_JOB_DATA:
+            return "Edit Job Data";
+        case Priorities.LOAD_RAW_TUBES:
+            return "Load Raw Tubes";
+        case Priorities.ENTER_BATCH_DATA:
+            return "Enter Batch Data";
         case Priorities.START_POT_PRESSURIZATION:
             return "Start Pot Pressurization";
+        case Priorities.PRIME_APPLICATOR:
+            return "Prime Applicator";
         case Priorities.PRE_WEIGH_TUBES:
             return "Pre Weigh Tubes";
         case Priorities.APPLY_LINER:
             return "Apply Liner";
+        case Priorities.BRUSH_LINER:
+            return "Brush Liner";
         case Priorities.PHOTOGRAPH_TUBES:
             return "Photograph Tubes";
         case Priorities.POST_WEIGH_TUBES:
             return "Post Weigh Tubes";
-        case Priorities.START_POT_DEPRESSURIZATION:
-            return "Start Pot Depressurization";
-        case Priorities.WAIT_IMAGE_INSPECTION:
-            return "Wait Image Inspection";
+        case Priorities.PREP_FOR_UNLOAD:
+            return "Prep For Unload";
+        case Priorities.REVIEW_VIDEOS:
+            return "Review Videos";
+        case Priorities.SAVE_BATCH_DATA:
+            return "Save Batch Data";
         case Priorities.UNLOAD_PARTS:
             return "Unload Parts";
+        case Priorities.DONE:
+            return "Done";
         default:
             return "Unknown";
     }
