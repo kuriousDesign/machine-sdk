d7ada5f09bc00c69137a97809e092ed6d06c71a9
kuriousdesign <gardner.761@gmail.com>
2026-07-22 17:50:55 -0600

    updated device ids

--- src/enums/DeviceId.ts
+++ src/enums/DeviceId.ts
@@ -1,13 +1,13 @@
 export enum DeviceId {
-    NONE = 0,       // Represents no device selected or invalid state
-    SYS = 1,
-    HMI = 2,
-    SFTY = 3,
-    CON = 4,
-    DIAG = 5,
-    UDP = 6, //Robot Controller
-    ROB = 10,
-    //ABB = 11,
-    //EOAT = 12,
-    VIS = 13,
+	NONE = 0,       // Represents no device selected or invalid state
+	SYS = 1,
+	HMI = 2,
+	SFTY = 3,
+	CON = 4,
+	//DIAG = 5,
+	VIS = 5, // do not change this, its hard coded in the vision python service
+	UDP = 6, //Robot Controller
+	ROB = 7
+	
+	//COUNT = GCs.NUM_DEVICES //if a new device is added please ensure GCs.NUM_DEVICES is larger or equal to total, this number should match
 }
